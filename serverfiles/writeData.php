7038fc147d99e1f9a4fecca2943b16ff100a52b0
BartosP <55179109+BartosP@users.noreply.github.com>
2019-11-12 18:41:52 +0100

    Delete writeData.php

--- serverfiles/writeData.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $name = $_GET["name"];
-    $surname = $_GET["surname"];
-    $fileContent = ";".$name.";".$surname.";";
-    file_put_contents("data.txt", $fileContent);
-    header("Location: index.html");
-    die();
-?>
